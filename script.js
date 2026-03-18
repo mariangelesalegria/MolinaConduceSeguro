ac6cb780b2bfc3d77d69dbb7a9f61500da1c704f
mariangelesalegria <4327040@alu.murciaeduca.es>
2026-03-18 10:39:48 +0000

    Refactor user registration and navigation functions

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function irRegistro(){
-    window.location.href="registro.html";
-}
+function registrarUsuario(e){
+    e.preventDefault();
 
-function irMapa(){
-    window.location.href="mapa.html";
+    let nombre = document.querySelector("input[type='text']").value;
+    let email = document.querySelector("input[type='email']").value;
+    let puntos = Math.floor(Math.random() * 1000);
+
+    let usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+    let nuevoUsuario = {
+        nombre: nombre,
+        email: email,
+        puntos: puntos
+    };
+
+    usuarios.push(nuevoUsuario);
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+
+    // GUARDAMOS EL USUARIO ACTUAL
+    localStorage.setItem("usuarioActual", JSON.stringify(nuevoUsuario));
+
+    alert("Usuario registrado correctamente");
+
+    window.location.href = "perfil.html"; // ahora va al perfil
+}
