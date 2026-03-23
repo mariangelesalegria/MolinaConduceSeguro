780d0780f8eaaada7e75b564757e03b3b24c6633
mariangelesalegria <4327040@alu.murciaeduca.es>
2026-03-23 14:40:33 +0100

    Update perfil.js

--- perfil.js
+++ perfil.js
@@ -1,20 +1,23 @@
 let usuario = localStorage.getItem("usuarioActual");
 
 if(usuario){
+
     usuario = JSON.parse(usuario);
 
     document.getElementById("nombre").textContent = usuario.nombre;
     document.getElementById("email").textContent = usuario.email;
     document.getElementById("puntos").textContent = usuario.puntos;
 
-    document.getElementById("fotoPerfil").src = usuario.foto || "default.png";
-
     let nivel = "Bronce";
-    if(usuario.puntos>1000) nivel="Oro";
-    else if(usuario.puntos>500) nivel="Plata";
+
+    if(usuario.puntos > 1000){
+        nivel = "Oro";
+    } else if(usuario.puntos > 600){
+        nivel = "Plata";
+    }
 
     document.getElementById("nivel").textContent = nivel;
 
 } else {
-    document.body.innerHTML="<h2>No hay usuario registrado</h2>";
+    document.body.innerHTML = "<h2>No hay usuario guardado</h2>";
 }
